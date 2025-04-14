f08c51ed123155e3a771163e73e12163e0baa547
Vytor Oliveira <156971837+Vytor-Oliveira@users.noreply.github.com>
2025-04-14 15:08:58 +0000

    teste de jwt 2.0

--- js/index.js
+++ js/index.js
@@ -31,46 +31,6 @@ function verDetalhes(produtoId) {
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
-  form.addEventListener("submit", async function (event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    const confirmarSenha = document.getElementById("confirmar-senha").value;
-
-    if (senha !== confirmarSenha) {
-      alert("As senhas não coincidem!");
-      return;
-    }
-
-    try {
-      const response = await fetch("https://puffwear.up.railway.app/cadastro", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome, email, senha }),
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        alert("Cadastro realizado com sucesso!");
-        window.location.href = "login.html"; // Redireciona para login
-      } else {
-        alert(`Erro: ${data.message}`);
-      }
-    } catch (error) {
-      console.error("Erro ao cadastrar:", error);
-      alert("Erro ao conectar com o servidor. Tente novamente.");
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-
   // Detecta qual página está aberta
   const isCadastro = window.location.pathname.includes("cadastro.html");
   const isLogin = window.location.pathname.includes("login.html");
