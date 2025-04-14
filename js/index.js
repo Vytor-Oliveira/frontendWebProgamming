f3a4ef0847e2f7b10f9c1da12169a13a9b03f4ae
Vytor Oliveira <156971837+Vytor-Oliveira@users.noreply.github.com>
2025-04-14 23:21:50 +0000

    test bug

--- js/index.js
+++ js/index.js
@@ -25,7 +25,45 @@ function moverCarrossel(direcao) {
 
 // Função para acessar a página do produto
 
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
 
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const confirmarSenha = document.getElementById("confirmar-senha").value;
+
+    if (senha !== confirmarSenha) {
+      alert("As senhas não coincidem!");
+      return;
+    }
+
+    try {
+      const response = await fetch("https://puffwear.up.railway.app/cadastro", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({nome, email, senha}),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Cadastro realizado com sucesso!");
+        window.location.href = "./login.html"; // Redireciona para login
+      } else {
+        alert(`Erro: ${data.message}`);
+      }
+    } catch (error) {
+      console.error("Erro ao cadastrar:", error);
+      alert("Erro ao conectar com o servidor. Tente novamente.");
+    }
+  });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("form-login");
